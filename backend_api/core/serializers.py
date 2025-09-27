72ccd467d1d3649e58964b8c13e945eda9bfd2f6
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-09-27 11:24:43 +0530

    Coded models in backend and made migrations

---
+++ backend_api/core/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import Bike, Event, BikeImage
+from django.contrib.auth.models import User
+
+class BikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bike
+        fields = "__all__"
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bike
+        fields = "__all__"
+class BikeImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bike
+        fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email"]
--- backend_api/core/views.py
+++ backend_api/core/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Bike, Event, BikeImage
+from .serializers import BikeSerializer, UserSerializer, EventSerializer, BikeImageSerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class BikeViewSet(viewsets.ModelViewSet):
+    queryset = Bike.objects.all()
+    serializer_class = BikeSerializer
+    permission_classes = [IsAuthenticated]
+
+class BikeViewSet(viewsets.ModelViewSet):
+    queryset = Bike.objects.all()
+    serializer_class = BikeSerializer
+    permission_classes = [IsAuthenticated]
+
+class BikeImageViewSet(viewsets.ModelViewSet):
+    queryset = Bike.objects.all()
+    serializer_class = BikeSerializer
+    #permission_classes = [IsAuthenticated]
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
