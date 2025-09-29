a0f39037448e9e26a65de506e8c69d3c6b506bfa
HerowithSkillz <153258296+HerowithSkillz@users.noreply.github.com>
2025-09-29 11:57:33 +0530

    Understood serializers

--- backendOnBikes/backend_api/api/views.py
+++ backendOnBikes/backend_api/api/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
 from core.models import Bike
+from .serializers import BikeSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
 # Create your views here.
+@api_view(['GET', 'POST'])
 def bikesView(request):
-    bikes = Bike.objects.all()
-    bikes_list = list(bikes.values())
-    return JsonResponse(bikes_list, safe=False)
+    if request.method == 'GET':
+        #Get all the data from bikes table
+        bikes = Bike.objects.all()
+        serializer = BikeSerializer(bikes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BikeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return  Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
--- backendOnBikes/backend_api/core/serializers.py
+++ backendOnBikes/backend_api/core/serializers.py
@@ -1,22 +1,22 @@
-from rest_framework import serializers
-from .models import Bike, Event, BikeImage
-from django.contrib.auth.models import User
+# from rest_framework import serializers
+# from .models import Bike, Event, BikeImage
+# from django.contrib.auth.models import User
 
-class BikeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Bike
-        fields = "__all__"
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = "__all__"
+# class BikeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Bike
+#         fields = "__all__"
+# class EventSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Event
+#         fields = "__all__"
 
-class BikeImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BikeImage
-        fields = "__all__"
+# class BikeImageSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = BikeImage
+#         fields = "__all__"
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email"]
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ["id", "username", "email"]
