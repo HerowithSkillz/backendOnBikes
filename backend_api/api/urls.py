3340ef274d26fd4b1182909022f09992594454a3
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-09-29 22:19:47 +0530

    Used serializer and implemented for GET

--- backend_api/api/urls.py
+++ backend_api/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bikes/', views.bikesView)
+    path('bikes/', views.bikesView),
+    path('bikes/<int:pk>/', views.bikeDetailView )
 ]
 #video timestamp: 36:47
--- backend_api/api/views.py
+++ backend_api/api/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
 from core.models import Bike
+from .serializers import BikeSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-
+@api_view(['GET', 'POST'])
 # Create your views here.
 def bikesView(request):
-    bikes = Bike.objects.all()
-    bikes_list = list(bikes.values())
-    return JsonResponse(bikes_list, safe=False)
+    if request.method == 'GET':
+        #Get all the data from the student table.
+        bikes = Bike.objects.all()
+        serializer = BikeSerializer(bikes, many=True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = BikeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET'])
+def bikeDetailView(request, pk):
+    try:
+        bike = Bike.objects.get(pk=pk)
+    except Bike.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = BikeSerializer(bike)
+        return Response(serializer.data ,status=status.HTTP_200_OK)
+    
