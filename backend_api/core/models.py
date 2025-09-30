f7ba6c12df96277efacbd8c0e5a88b460dcb057f
Vignesh Anil Thandessary <heroskillz910@gmail.com>
2025-09-30 11:56:20 +0530

    Copied the frontend into the main_project folder BACKENDONBIKES

--- backend_api/core/models.py
+++ backend_api/core/models.py
@@ -1,34 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Bike(models.Model):
-    # owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bikes')
-    # image = models.ImageField(upload_to='bikes/', blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bikes')
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     year = models.IntegerField()
+    image = models.ImageField(upload_to='bikes/', blank=True, null=True)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.brand} {self.name} ({self.year})"
+    
+    # def get_absolute_url(self):
+    #     return reverse('bike-detail', kwargs={'pk': self.pk})
 
-# class Event(models.Model):
-#     host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hosted_events')
-#     title = models.CharField(max_length=200)
-#     location = models.CharField(max_length=200)
-#     date = models.DateTimeField()
-#     description = models.TextField()
-#     participants = models.ManyToManyField(User, related_name='events_participating', blank=True)
-#     created_at = models.DateTimeField(auto_now_add=True)
+class Event(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE, related_name='hosted_events')
+    title = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    description = models.TextField()
+    participants = models.ManyToManyField(User, related_name='events_participating', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-#     def __str__(self):
-#         return self.title
+    def __str__(self):
+        return self.title
 
-# class BikeImage(models.Model):
-#     bike = models.ForeignKey('Bike', on_delete=models.CASCADE, related_name='additional_images')
-#     image = models.ImageField(upload_to='bikes/')
-#     caption = models.CharField(max_length=200, blank=True)
-#     uploaded_at = models.DateTimeField(auto_now_add=True)
+    # def get_absolute_url(self):
+    #     return reverse('event-detail', kwargs={'pk': self.pk})
 
-#     def __str__(self):
-#         return f"Image for {self.bike.name}"
+class BikeImage(models.Model):
+    bike = models.ForeignKey('Bike', on_delete=models.CASCADE, related_name='additional_images')
+    image = models.ImageField(upload_to='bikes/')
+    caption = models.CharField(max_length=200, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for {self.bike.name}"
